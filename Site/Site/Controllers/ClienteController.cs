542c259c06e665592561efa30a541f3550c7c388
Christian_Anjos <christiananjos@hotmail.com>
2023-07-01 14:11:55 -0300

    Criação pagina Detalhes e linkei (temporariamente) as actiions para os botões.

--- Site/Site/Controllers/ClienteController.cs
+++ Site/Site/Controllers/ClienteController.cs
@@ -8,5 +8,25 @@ namespace Site.Controllers
         {
             return View();
         }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Detalhe()
+        {
+            return View();
+        }
+
+        public IActionResult Remover()
+        {
+            return View();
+        }
     }
 }
