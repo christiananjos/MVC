8cc5852b370f53f22e35b3ac30302357904a4f29
Christian_Anjos <christiananjos@hotmail.com>
2023-07-02 03:35:30 -0300

    configuração de Context e Injeção de Dependencia do EF

---
+++ Site/Site/Data/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Site.Models;
+
+namespace Site.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
--- Site/Site/Program.cs
+++ Site/Site/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Site.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddEntityFrameworkSqlServer().AddDbContext<Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
+
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -11,6 +18,9 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
