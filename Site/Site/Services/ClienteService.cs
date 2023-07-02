fe3db66b4ef8b8b3c92758fce6c8e92a8d7177ca
Christian_Anjos <christiananjos@hotmail.com>
2023-07-02 04:46:22 -0300

    Implementação ClienteService e UsuarioService

---
+++ Site/Site/Services/ClienteService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Site.Models;
+using Site.Services.Interfaces;
+
+namespace Site.Services
+{
+    public class ClienteService : BaseService<Cliente>, IClienteService
+    {
+        private ModelStateDictionary _modelState;
+
+        public ClienteService(ModelStateDictionary modelState)
+        {
+            _modelState = modelState;
+        }
+
+        public bool ValidarExistente(Cliente item)
+        {
+            if (item.Id.ToString() == null)
+            {
+                if (List().Where(c => c.Email == item.Email).Count() > 0)
+                    _modelState.AddModelError("Email", "Cliente com esse e-mail já cadastrado");
+            }
+
+            return _modelState.IsValid;
+        }
+    }
+}
---
+++ Site/Site/Services/UsuarioService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Site.Models;
+using Site.Services.Interfaces;
+using System.Data;
+
+namespace Site.Services
+{
+    public class UsuarioService : BaseService<Usuario>, IUsuarioService
+    {
+        private ModelStateDictionary _modelState;
+
+        public UsuarioService(ModelStateDictionary modelState)
+        {
+            _modelState = modelState;
+        }
+
+        public bool ValidarExistente(Usuario item)
+        {
+            if (item.Id.ToString() == null)
+            {
+                if (List().Where(c => c.Usernaname == item.Usernaname).Count() > 0)
+                    _modelState.AddModelError("Usernaname", "Usuario já cadastrado");
+            }
+
+            return _modelState.IsValid;
+        }
+    }
+}
