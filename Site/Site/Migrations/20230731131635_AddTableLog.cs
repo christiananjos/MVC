46c4a94130f3a91e50c8baf05d7fa8d55f1efcb0
Christian_Anjos <christiananjos@hotmail.com>
2023-07-31 10:17:08 -0300

    migrstion removendo DTLog da Tabela Log

---
+++ Site/Site/Migrations/20230731131635_AddTableLog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Site.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTableLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DtLog",
+                table: "Logs");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DtLog",
+                table: "Logs",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
--- Site/Site/Models/Log.cs
+++ Site/Site/Models/Log.cs
@@ -2,8 +2,13 @@
 {
     public class Log : Base
     {
+        public Log(string description, string user)
+        {
+            Description = description;
+            User = user;
+        }
+
         public string Description { get; set; }
         public string User { get; set; }
-        public DateTime DtLog { get; set; }
     }
 }
