9ea380959b6006dc6341649b9b7da3e505639f50
Christian_Anjos <christiananjos@hotmail.com>
2023-08-01 17:54:22 -0300

    Migration AlterTable_UsuarioVerificacao

---
+++ Site/Site/Migrations/20230801205322_AlterTable_UsuarioVerificacao.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Site.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterTable_UsuarioVerificacao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_UsuarioVerificacoes_UsuarioId",
+                table: "UsuarioVerificacoes",
+                column: "UsuarioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UsuarioVerificacoes_Usuarios_UsuarioId",
+                table: "UsuarioVerificacoes",
+                column: "UsuarioId",
+                principalTable: "Usuarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UsuarioVerificacoes_Usuarios_UsuarioId",
+                table: "UsuarioVerificacoes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UsuarioVerificacoes_UsuarioId",
+                table: "UsuarioVerificacoes");
+        }
+    }
+}
--- Site/Site/Models/UsuarioVerificacao.cs
+++ Site/Site/Models/UsuarioVerificacao.cs
@@ -1,8 +1,12 @@
-﻿namespace Site.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Site.Models
 {
     public class UsuarioVerificacao : Base
     {
+        [Display(Name = "UsuarioId")]
         public Guid UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
         public string Code { get; set; }
     }
 }
